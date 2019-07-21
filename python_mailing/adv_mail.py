a3c7a177df514e26195d583132fecc6504f83a30
Sami_aZeus <sami.ezzerouali@hotmail.com>
2019-07-21 13:16:24 +0200

    attachment

--- python_mailing/adv_mail.py
+++ python_mailing/adv_mail.py
@@ -1,11 +1,12 @@
 import smtplib
-
+import imghdr
 import config
 from email.message import EmailMessage
 
 r_mail='sami.ezzerouali@hotmail.com'
 r_subject = 'Python test !'
 r_message = 'test 2'
+r_joint_src = './ressources/1.png'
 
 def send(msg):
     with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
@@ -18,6 +19,14 @@ def main():
     msg['From'] = config.EMAIL_ADDRESS
     msg['To'] = r_mail
     msg.set_content(r_message)
+    
+    with open(r_joint_src,'rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+        print(file_type)
+        #msg.set_content(r_message)
+    msg.add_attachment(file_data,maintype='image',subtype=file_type,filename=file_name)
     send(msg)
 
 main()
