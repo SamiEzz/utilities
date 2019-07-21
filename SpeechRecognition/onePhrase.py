8a7363ebf9c28b309c9dd1fa00cf4bfaebe9a7e1
Sami_aZeus <sami.ezzerouali@hotmail.com>
2019-07-21 17:55:30 +0200

    AZEUS

--- SpeechRecognition/onePhrase.py
+++ SpeechRecognition/onePhrase.py
@@ -1,14 +1,9 @@
-import pyttsx3
 import speech_recognition as sr
-
-engine = pyttsx3.init()
 r = sr.Recognizer()
-
 with sr.Microphone() as source:                # use the default microphone as the audio source
     audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
 
 try:
-    engine.say(r.recognize_google(audio, language="fr-FR"))
-    engine.runAndWait()
+    print("You said " + r.recognize_google(audio))    # recognize speech using Google Speech Recognition
 except LookupError:                            # speech is unintelligible
     print("Could not understand audio")
