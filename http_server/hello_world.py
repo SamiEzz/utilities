398245729892289a69a0658cba18d610e1189312
Sami_amadeus_linux <sezzerouali@hotmail.com>
2019-11-07 22:02:01 +0100

    server

--- http_server/hello_world.py
+++ http_server/hello_world.py
@@ -5,6 +5,6 @@ PORT = 80
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("127.3.1.2", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
